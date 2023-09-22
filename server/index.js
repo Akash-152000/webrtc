1e3d95f3472f24db8bbadaed3f33047c7a376c60
Akash-152000 <akash152000akash@gmail.com>
2023-09-22 15:25:06 +0530

    basic server setup

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const {Server} = require('socket.io')
+
+const io = new Server();
+const app = express();
+app.use(bodyParser.json())
+
+io.on('connection',(socket)=>{
+    console.log("Connected")
+})
+
+app.listen(8000,()=>console.log("Server running at port 8000"))
+io.listen(8001)
