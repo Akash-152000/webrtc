14ad69146c4c0ba3cdde67e05a79e3b2496adeeb
Akash-152000 <akash152000akash@gmail.com>
2023-09-22 18:47:40 +0530

    Got info from ice servers and created offer or SDP

---
+++ client/src/providers/Peer.jsx
@@ -0,0 +1,31 @@
+const { createContext, useMemo } = require("react");
+
+const PeerContext = createContext();
+
+export const PeerProvider = (props)=>{
+
+    const peer = useMemo(()=>{
+        const peer = new RTCPeerConnection({        // rtcpeerconnection gives back info about our device
+            iceServers:[
+                {
+                    urls:[
+                        "stun:stun.l.google.com:19302",
+                        "stun:global.stun.twilio.com:3478"
+                    ]
+                }
+            ]
+        },[])
+    })
+
+    const createOffer =async ()=>{
+        const offer = await peer.createOffer();     // offer is created
+        await peer.setLocalDescription(offer);      // offer needs to be stored on our local state
+        return offer
+    }
+
+    return(
+        <PeerContext.Provider value={{peer, createOffer}}>
+            {props.children}
+        </PeerContext.Provider>
+    )
+}
