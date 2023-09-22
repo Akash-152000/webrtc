6d6b6c4e12d7bc399d26685cd294e195efa7b86b
Akash-152000 <akash152000akash@gmail.com>
2023-09-22 16:09:53 +0530

    Implemented routers setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,11 @@
 
+import { Outlet } from 'react-router-dom'
 import './App.css'
 
 function App() {
   return (
     <>
-      <h1>Helllo</h1>
+      <Outlet/>
     </>
   )
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {Route, RouterProvider, createBrowserRouter, createRoutesFromElements} from 'react-router-dom'
+
+
+const router = createBrowserRouter(
+      createRoutesFromElements(
+        <Route path='/' element={<App />}>
+          <Route path='' element={<h1>Helllllllo</h1>}/>
+        </Route>
+      )
+  )
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
