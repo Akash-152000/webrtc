1cc9b4e9b885f63a06eed24864b0bc1c0e121cc8
Akash-152000 <akash152000akash@gmail.com>
2023-09-22 17:28:17 +0530

    added form logic

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,18 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {useSocket} from '../providers/Socket'
 
 export const Home = () => {
   const {socket} = useSocket()
-  console.log(socket);
 
-  socket.emit('join-room',{roomId:'1',email:'a@export.com'})
+  const [email,setEmail] = useState('')
+  const [roomId,setRoomId] = useState('')
+
+  const handleRoomJoin=()=>{
+    socket.emit('join-room',{email,roomId})
+  }
+
 
   return (
     <div className='home-container'>
         <div className='container'>
-            <input type="email" placeholder='Enter your email' />
-            <input type="roomId" placeholder='Enter Room Id' />
-            <button>Enter Room</button>
+            <input value={email} onChange={e=>setEmail(e.target.value)} type="email" placeholder='Enter your email' />
+            <input value={roomId} onChange={e=>setRoomId(e.target.value)} type="roomId" placeholder='Enter Room Id' />
+            <button onClick={handleRoomJoin}>Enter Room</button>
         </div>
     </div>
   )
